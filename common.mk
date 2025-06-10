d152780a3bf770cb468c05c9b9b29b89becb29f0
Monkey <yccd@gmail.com>
2025-06-10 08:54:25 +0800

    sm8550-common: Use frameworks one instead

--- common.mk
+++ common.mk
@@ -374,6 +374,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
+# Touchscreen
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
