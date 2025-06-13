61c9b04f18706e1c014ee68a04edc4e6f0425f1b
xt0032rus <andrey.denikin9@mail.com>
2025-06-13 21:02:56 +0300

    Revert "sm8550-common: Implement turbo charging V1"
    This reverts commit eb29b52820707c17cdbbc184b55f7e2b80abcb22.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -38,7 +38,6 @@ import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingService;
 import org.lineageos.settings.touchsampling.TouchSamplingTileService;
-import org.lineageos.settings.turbocharging.TurboChargingService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -102,10 +101,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Start Pocket Mode Service
         PocketService.startService(context);
-
-        // Start TurboChargingService
-        Intent turboChargingIntent = new Intent(context, TurboChargingService.class);
-        context.startService(turboChargingIntent);
 
         // Start Power Profile Tile Service
         context.startServiceAsUser(new Intent(context, PowerProfileTileService.class), UserHandle.CURRENT);
